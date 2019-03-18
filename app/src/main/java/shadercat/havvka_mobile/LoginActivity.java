bbcef734dab7917aed7789435dffae3f2b3a2863
Tkhanh Tran <shadercat@outlook.com>
2019-03-18 20:29:39 +0200

    Added animation and input fields
    - added animation for logo;
    - added input fields for sign in and register tab;
    - convert pictures of logo and crutch to VectorDrawable;

--- app/src/main/java/shadercat/havvka_mobile/LoginActivity.java
+++ app/src/main/java/shadercat/havvka_mobile/LoginActivity.java
@@ -4,7 +4,9 @@ import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 //activity for login (sing up, sing in or guest)
@@ -15,26 +17,43 @@ public class LoginActivity extends AppCompatActivity {
     Button Register;
     Button Guest;
     RelativeLayout LoginLayout;
+    RelativeLayout SignInUI;
+    RelativeLayout LoginUI;
+    RelativeLayout RegisterUI;
+    ImageView Logo;
     AnimationDrawable Background_Animation;
+    boolean IsMainScreen = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //get elements from layout
         Sign_in = (Button) findViewById(R.id.activity_login_sign_in);
         Register = (Button) findViewById(R.id.activity_login_register);
         Guest = (Button) findViewById(R.id.activity_login_guest);
         LoginLayout = (RelativeLayout) findViewById(R.id.activity_login);
-        Background_Animation = (AnimationDrawable) LoginLayout.getBackground();
+        SignInUI = (RelativeLayout) findViewById(R.id.activity_login_sign_in_ui);
+        LoginUI = (RelativeLayout) findViewById(R.id.activity_login_ui);
+        RegisterUI = (RelativeLayout) findViewById(R.id.activity_login_register_ui);
+        Logo = (ImageView) findViewById(R.id.activity_login_logo);
 
+        //set animation for background
+        Background_Animation = (AnimationDrawable) LoginLayout.getBackground();
         Background_Animation.setEnterFadeDuration(3000);
         Background_Animation.setExitFadeDuration(3000);
         Background_Animation.start();
+
+        //set animation for logo
+        Logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.logo_anim));
 
         Sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //code for button sign in (when pressed)
+                IsMainScreen = false;
+                LoginUI.setVisibility(View.INVISIBLE);
+                SignInUI.setVisibility(View.VISIBLE);
             }
         });
 
@@ -42,6 +61,9 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //code for button register (when pressed)
+                IsMainScreen = false;
+                LoginUI.setVisibility(View.INVISIBLE);
+                RegisterUI.setVisibility(View.VISIBLE);
             }
         });
 
@@ -51,5 +73,19 @@ public class LoginActivity extends AppCompatActivity {
                 //code for button guest (when pressed)
             }
         });
+    }
+    @Override
+    public void onBackPressed() {
+        if(IsMainScreen)
+        {
+            super.onBackPressed();
+        }
+        else
+        {
+            IsMainScreen = true;
+            LoginUI.setVisibility(View.VISIBLE);
+            SignInUI.setVisibility(View.INVISIBLE);
+            RegisterUI.setVisibility(View.INVISIBLE);
+        }
     }
 }
